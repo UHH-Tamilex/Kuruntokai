159e2a189c13e4f884daaff843a4493b973dd897
chchch <charles.li@cnrs.fr>
2023-12-05 16:16:45 +0100

    work better with rdgGrp

--- _indexer/index.mjs
+++ _indexer/index.mjs
@@ -125,9 +125,10 @@ const alignVariants = (doc,alignment) => {
         let textslice = textarr.slice(newstart,newend);
         let splitslice = wordsplitarr.slice(newstart,newend);
         let readings = [];
-        for(const rdg of variant.querySelectorAll(':scope > rdg')) {
-            
-            const rdgtext = cleanText(rdg);
+        for(const rdg of variant.querySelectorAll(':scope > rdg, :scope > rdgGrp')) {
+            const rdgtext = rdg.tagName === 'rdgGrp' ?
+                cleanText(rdg.querySelector('rdg[type="main"]')) :
+                cleanText(rdg);
             const witnesses = rdg.getAttribute('wit').split(' ').map(s => {
                 if(correctedWits.includes(s))
                     return s.slice(0,-1) + 'v';
